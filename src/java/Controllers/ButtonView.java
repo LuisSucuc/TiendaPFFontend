b1d8097949fa77a501bc98110011b3f1892fb02f
Luis Sucuc <jlsucuc@gmail.com>
2018-11-01 18:15:10 -0600

    first commit

---
+++ src/java/Controllers/ButtonView.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+ 
+import CAD.UserCAD;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+@ManagedBean
+public class ButtonView {
+     
+    public void buttonAction(ActionEvent actionEvent) throws Exception {
+        UserCAD test  = new UserCAD();
+        test.validarUsuer("LUIS");
+        addMessage("Welcome to Primefaces!!");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
---
+++ src/java/Controllers/ProductoController.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import CAD.ProductoCAD;
+import DTO.Producto;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author luis
+ */
+
+@ManagedBean(name="productoController")
+@ViewScoped
+public class ProductoController  implements Serializable{
+    
+    
+    
+    
+    private List<Producto> listaProductos;
+    ProductoCAD productoCAD =  new ProductoCAD();
+    
+    @PostConstruct
+    public void init(){
+        try {
+            this.listaProductos = productoCAD.getTodos();
+        } catch (Exception ex) {
+            System.out.println("Error al obtener productos");
+        }
+    }
+
+    public List<Producto> getListaProductos() {
+        return listaProductos;
+    }
+    
+    public void listar(){
+        try {
+            this.listaProductos = productoCAD.getTodos();
+        } catch (Exception ex) {
+            System.out.println("Error al obtener productos (LISTAR)");
+        }
+    }
+    
+    private Producto producto = new Producto();
+    
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+    
+  
+    public void nuevoProducto() throws Exception{
+        productoCAD.insertar(producto);
+        listar();
+        
+    }
+    
+    public Producto leerIDProducto(int id) {
+        System.out.println("GET ID");
+        System.out.println(id);
+        try {
+            this.producto = productoCAD.getProductoID(id);
+        } catch (Exception ex) {
+            System.out.println("Error al obtener producto por ID");
+        }
+     
+        return this.producto;
+    }
+    
+    public void modificar() throws Exception{
+        
+        productoCAD.modificar(producto);
+        listar();
+        
+    }
+    
+     public void eliminar(int id) {
+        productoCAD.eliminar(id);
+        listar();
+    }
+    
+   
+     
+     
+     
+}
